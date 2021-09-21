bbcbbb653e927f8afceff0fa9685caedfc1af99a
terakawaakari <terakawaakari0701@gmail.com>
2021-09-21 21:00:51 +0900

    first commit

---
+++ src/main/java/jp/co/f1/spring/bms/Bmsspring05Application.java
@@ -0,0 +1,13 @@
+package jp.co.f1.spring.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bmsspring05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bmsspring05Application.class, args);
+	}
+
+}
